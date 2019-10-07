e1c9216d3d7e3ea56594cd1fcf939aa3198721ae
jczic <jczic@free.fr>
2019-10-07 03:06:10 +0200

    Add a chat

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 from MicroWebSrv2  import *
 from XAsyncSockets import XAsyncSocketsPool
 from time          import sleep
+from _thread       import allocate_lock
 
 # ============================================================================
 # ============================================================================
@@ -12,7 +13,9 @@ from time          import sleep
 def RequestTestRedirect(microWebSrv2, request) :
     request.Response.ReturnRedirect('/test.pdf')
 
-# ------------------------------------------------------------------------
+# ============================================================================
+# ============================================================================
+# ============================================================================
 
 @WebRoute(GET, '/test-post', name='TestPost1/2')
 def RequestTestPost(microWebSrv2, request) :
@@ -61,18 +64,25 @@ def RequestTestPost(microWebSrv2, request) :
             MicroWebSrv2.HTMLEscape(lastname) )
     request.Response.ReturnOk(content)
 
-# ------------------------------------------------------------------------
+# ============================================================================
+# ============================================================================
+# ============================================================================
 
 def OnWebSocketAccepted(microWebSrv2, webSocket) :
-    print('WebSocket Accepted :')
+    print('Example WebSocket accepted:')
     print('   - User   : %s:%s' % webSocket.Request.UserAddress)
     print('   - Path   : %s'    % webSocket.Request.Path)
     print('   - Origin : %s'    % webSocket.Request.Origin)
-    webSocket.OnTextMessage   = OnWebSocketTextMsg
-    webSocket.OnBinaryMessage = OnWebSocketBinaryMsg
-    webSocket.OnClosed        = OnWebSocketClosed
+    if webSocket.Request.Path.lower() == '/wschat' :
+        WSJoinChat(webSocket)
+    else :
+        webSocket.OnTextMessage   = OnWebSocketTextMsg
+        webSocket.OnBinaryMessage = OnWebSocketBinaryMsg
+        webSocket.OnClosed        = OnWebSocketClosed
 
-# ------------------------------------------------------------------------
+# ============================================================================
+# ============================================================================
+# ============================================================================
 
 def OnWebSocketTextMsg(webSocket, msg) :
     print('WebSocket text message: %s' % msg)
@@ -87,6 +97,45 @@ def OnWebSocketBinaryMsg(webSocket, msg) :
 
 def OnWebSocketClosed(webSocket) :
     print('WebSocket %s:%s closed' % webSocket.Request.UserAddress)
+
+# ============================================================================
+# ============================================================================
+# ============================================================================
+
+global _chatWebSockets
+_chatWebSockets = [ ]
+
+global _chatLock
+_chatLock = allocate_lock()
+
+# ------------------------------------------------------------------------
+
+def WSJoinChat(webSocket) :
+    webSocket.OnTextMessage = OnWSChatTextMsg
+    webSocket.OnClosed      = OnWSChatClosed
+    with _chatLock :
+        for ws in _chatWebSockets :
+            wsIP = ws.Request.UserAddress[0]
+            ws.SendTextMessage('<%s has joined the chat>' % wsIP)
+        _chatWebSockets.append(webSocket)
+
+# ------------------------------------------------------------------------
+
+def OnWSChatTextMsg(webSocket, msg) :
+    with _chatLock :
+        for ws in _chatWebSockets :
+            wsIP = ws.Request.UserAddress[0]
+            ws.SendTextMessage('<%s> %s' % (wsIP, msg))
+
+# ------------------------------------------------------------------------
+
+def OnWSChatClosed(webSocket) :
+    with _chatLock :
+        if webSocket in _chatWebSockets :
+            _chatWebSockets.remove(webSocket)
+        for ws in _chatWebSockets :
+            wsIP = ws.Request.UserAddress[0]
+            ws.SendTextMessage('<%s left the chat>' % wsIP)
 
 # ============================================================================
 # ============================================================================
