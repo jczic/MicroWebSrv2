81f66d08a53725fa85439ae15877a47446e9a178
jczic <jczic.bos@gmail.com>
2024-03-09 13:17:00 +0100

    New release v2.1.0

--- MicroWebSrv2/__init__.py
+++ MicroWebSrv2/__init__.py
@@ -5,11 +5,11 @@ Copyright © 2024 Jean-Christophe Bos (jczic.bos@gmail.com)
 """
 
 print()
-print('       ---------------------------')
-print('       - Python pkg MicroWebSrv2 -')
-print('       -      version 2.1.0      -')
-print('       -        by JC`zic        -')
-print('       ---------------------------')
+print('       -------------------------')
+print('       -  Python MicroWebSrv2  -')
+print('       -     version 2.1.0     -')
+print('       -       by JC`zic       -')
+print('       -------------------------')
 print()
 
 from .libs.XAsyncSockets import *
